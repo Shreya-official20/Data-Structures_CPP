d8f3303cdf4c999b420f0b3a5c5b693e2697986c
ShreyaOfficial <shreyasingh202001@gmail.com>
2023-03-20 22:20:09 +0530

    pattern

---
+++ Patterns/Upattern11.cpp
@@ -0,0 +1,30 @@
+//diamond in square using if-else
+//         *
+//       * * *       
+//     * * * * *     
+//   * * * * * * *   
+//     * * * * *     
+//       * * *       
+//         *
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Number:";
+    cin>>n;
+    int n1 = n/2;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i+j <= n1 || j >= n1+i || i-j >= n1 || j >= (n-1+n1)-i){
+                cout<<"  ";
+            }
+            else{
+                cout<<"* ";
+            }
+        }
+        cout<<""<<endl;
+    }
+}
---
+++ Patterns/Upattern13.cpp
@@ -0,0 +1,30 @@
+//gmail pattern
+// * * * * * * * 
+// * *       * * 
+// *   *   *   * 
+// *     *     * 
+// *           * 
+// *           * 
+// * * * * * * *
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Number:";
+    cin>>n;
+    int n1 = n/2;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 || i == n-1 || j == n-1 || j==0 || (i==j && i<n1) || (i+j == n-1 && i<=n1) ){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<""<<endl;
+    }
+}
